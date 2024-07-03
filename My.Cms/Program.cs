0b41fbcf70edb6c175cd34c4205534c6b6f95ced
ドイツ <thd1152015@gmail.com>
2024-07-03 22:04:13 +0700

    Latest

---
+++ My.Cms/Program.cs
@@ -0,0 +1,30 @@
+using My.Cms.Extensions.Authentications;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    // My Custom
+    .AddBackOfficeKeycloakAuthentication()
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
